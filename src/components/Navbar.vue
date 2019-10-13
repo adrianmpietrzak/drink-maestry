b870f4416fadca191d18b4c315005e0e2f6181f6
Adrian Pietrzak <adrianmpietrzak@gmail.com>
2019-10-13 19:06:01 +0200

    Add navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="nav">
+    <div class="nav__menu">
+      <router-link class="nav__link" to="/">Home</router-link>
+      <router-link class="nav__link" to="/about">About</router-link>
+    </div>
+    <button class="nav__ingrediets">Ingredients</button>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.nav {
+  background-color: $first-color;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.nav__menu {
+  display: flex;
+}
+
+.nav__link {
+  color: $second-color;
+  text-decoration: none;
+  padding: 20px;
+
+  &:hover {
+    background-color: $second-color;
+    color: $first-color;
+  }
+}
+
+.nav__ingrediets {
+  display: flex;
+  padding: 20px;
+  border: none;
+  background: none;
+  color: $second-color;
+  cursor: pointer;
+
+  &:hover {
+    background-color: $second-color;
+    color: $first-color;
+  }
+}
+</style>
